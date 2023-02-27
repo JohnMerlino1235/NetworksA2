834955cd3666b5e5cd126eb75687ff3c7c8a08b2
Kelli Therrien <71950157+kellitherrien@users.noreply.github.com>
2023-02-27 15:42:50 -0500

    updating server to client

--- client/clientmake.mk
+++ client/clientmake.mk
@@ -1,8 +1,8 @@
 CC = g++
 CFLAGS = -g -Wall
 
-server: mydriver.o mylink.o mytransport.o mysocket.o app_server.o
-	$(CC) $(CFLAGS) mydriver.o mylink.o mytransport.o mysocket.o app_server.o -o server
+client: mydriver.o mylink.o mytransport.o mysocket.o app_client.o
+	$(CC) $(CFLAGS) mydriver.o mylink.o mytransport.o mysocket.o app_client.o -o client
 
 mydriver.o: mydriver.cpp mydriver.h
 	$(CC) $(CFLAGS) -c mydriver.cpp
@@ -16,7 +16,7 @@ mytransport.o: mytransport.cpp mytransport.h
 mysocket.o: mysocket.cpp mysocket.h
 	$(CC) $(CFLAGS) -c mysocket.cpp
 
-app_server.o: app_server.cpp
+app_client.o: app_client.cpp
 	$(CC) $(CFLAGS) -c app_client.cpp
 
 clean:
