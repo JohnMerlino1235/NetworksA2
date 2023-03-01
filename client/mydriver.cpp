db48808e078a66de90690cc065ca72890d8cd80f
Kelli Therrien <71950157+kellitherrien@users.noreply.github.com>
2023-02-28 20:40:20 -0500

    getting rid of old print statements

--- client/mydriver.cpp
+++ client/mydriver.cpp
@@ -19,8 +19,6 @@ void driver_send(SkBuf* buffer){
     /* write the content of the frame in the file
      * new line, so the next frame is written in the next line
      */
-    cout << "buffer->get_head()" << buffer->get_head() << endl;
-    cout << "buffer->get_head_length" << buffer->get_head_length() << endl;
     outfile.write(buffer->get_head(), buffer->get_head_length());
     outfile << endl;
     outfile.close();
